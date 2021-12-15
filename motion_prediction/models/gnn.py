1a118ac8309e0e49a0d0916542084858819e15f2
jacklbruck <jacklbruck@gmail.com>
2021-12-15 12:57:51 -0600

    results and final changes

--- motion_prediction/models/gnn.py
+++ motion_prediction/models/gnn.py
@@ -1,12 +1,14 @@
+import torch.nn.functional as F
 import torch.nn as nn
 import torch
 
-from fairmotion.data.amass_dip import SMPL_JOINT_MAPPING, SMPL_PARENTS, SMPL_NR_JOINTS
+from fairmotion.data.amass_dip import SMPL_PARENTS, SMPL_NR_JOINTS
 from torch_geometric.nn import GATConv
 
-class GraphEncoder(nn.Module):
-    def __init__(self, input_dim=3, hidden_dim=32, num_layers=1, device="cpu"):
-        super(GraphEncoder, self).__init__()
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim=3, hidden_dim=24, num_layers=1, device="cpu"):
+        super(Encoder, self).__init__()
         # Save parameters.
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
@@ -14,18 +16,10 @@ class GraphEncoder(nn.Module):
         self.edge_index = self.get_edge_index()
 
         # Preprocessing linear layer.
-        self.pre = nn.Sequential(nn.Linear(input_dim, hidden_dim), nn.Tanh())
+        self.pre = nn.Linear(input_dim, hidden_dim)
 
         # Graph layer.
-        self.gc = GATConv(in_channels=hidden_dim, out_channels=hidden_dim)
-
-        # Recurrent layer.
-        self.lstm = nn.LSTM(
-            input_size=hidden_dim * SMPL_NR_JOINTS,
-            hidden_size=hidden_dim * SMPL_NR_JOINTS,
-            num_layers=num_layers,
-            batch_first=True
-        )
+        self.gc = GATConv(in_channels=hidden_dim, out_channels=hidden_dim // 3, heads=3)
 
     def get_edge_index(self):
         return torch.LongTensor(
@@ -51,65 +45,69 @@ class GraphEncoder(nn.Module):
         dims = (N, L, J, A)
 
         X = X.reshape(N * L * J, A)
-        edge_index = self.edge_index.repeat(1, N * L) + (torch.arange(N * L) * J).unsqueeze(-1).repeat(1, len(self.edge_index.T)).flatten().to(self.device)
-        batch = torch.arange(N).repeat(L*J).reshape(L*J, -1).T.flatten().to(self.device)
+        edge_index = self.edge_index.repeat(1, N * L) + (
+            torch.arange(N * L) * J
+        ).unsqueeze(-1).repeat(1, len(self.edge_index.T)).flatten().to(self.device)
+        batch = (
+            torch.arange(N).repeat(L * J).reshape(L * J, -1).T.flatten().to(self.device)
+        )
 
         return X, edge_index, batch, dims
 
     def forward(self, src):
         # Process for graph input.
         X, edge_index, _, dims = self.batchify(src)
+        (N, L, J, A) = dims
 
         # Pass through graph neural netowrk.
-        X = self.pre(X)
-        X = self.gc(X, edge_index)
-        X = self.from_graph_seq(X, dims)
+        X = F.relu(self.pre(X))
+        X = F.relu(self.gc(X, edge_index))
+        X = X.reshape(N, L, J * self.hidden_dim)
 
-        return self.lstm(X)
+        return X
 
 
-class GraphDecoder(nn.Module):
-    def __init__(self, hidden_dim=32, output_dim=3, num_layers=1, device="cpu", lstm=None):
-        super(GraphDecoder, self).__init__()
+class Model(nn.Module):
+    def __init__(self, input_dim=3, hidden_dim=24, num_layers=1, device="cpu"):
+        super(Model, self).__init__()
         # Save parameters.
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
         self.device = device
 
-        # Recurrent layer.
-        self.dec = lstm if lstm is not None else nn.LSTM(
+        # Build layers.
+        self.enc = Encoder(
+            input_dim=input_dim,
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            device=device
+        )
+        self.rec = nn.LSTM(
             input_size=hidden_dim * SMPL_NR_JOINTS,
             hidden_size=hidden_dim * SMPL_NR_JOINTS,
-            num_layers=num_layers,
-            batch_first=True
+            num_layers=1,
+            batch_first=True,
         )
-        self.post = nn.Linear(hidden_dim * SMPL_NR_JOINTS, output_dim * SMPL_NR_JOINTS)
-
-    def forward(self, src, tgt, max_len=None, h=None, c=None, teacher_forcing_ratio=0.5):
-        max_len = max_len if max_len is not None else tgt.size(1)
-
-        out = src[:, -1].unsqueeze(1)
-        outs = torch.zeros(tgt.size(0), max_len, self.hidden_dim * SMPL_NR_JOINTS).to(self.device)
-        for t in range(max_len):
-            out, (h, c) = self.dec(out, (h, c))
-
-            outs[:, t] = out.squeeze(1)
-
-        return self.post(outs)
-
-
-class GraphModel(nn.Module):
-    def __init__(self, enc, dec):
-        super(GraphModel, self).__init__()
-
-        self.enc = enc
-        self.dec = dec
+        self.dec = nn.Linear(hidden_dim * SMPL_NR_JOINTS, input_dim * SMPL_NR_JOINTS)
 
     def init_weights(self):
         pass
 
     def forward(self, src, tgt, max_len=None, teacher_forcing_ratio=0.5):
-        out, (h, c) = self.enc(src)
-        out = self.dec(out, tgt, max_len=max_len, h=h, c=c)
+        # Encode input sequences.
+        out = self.enc(src)
+        tgt = self.enc(tgt)
+        _, (h, c) = self.rec(out)
 
-        return out
+        max_len = max_len if max_len is not None else tgt.size(1)
+
+        out = tgt[:, 0].unsqueeze(1)
+        outs = torch.zeros(tgt.size(0), max_len, self.hidden_dim * SMPL_NR_JOINTS).to(
+            self.device
+        )
+        for t in range(max_len):
+            out, (h, c) = self.rec(out, (h, c))
+
+            outs[:, t] = out.squeeze(1)
+
+        return self.dec(outs)
